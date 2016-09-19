d5855f2f4975dc477da9d6e42c7d07e865865598
CRUZER-PC\MAHMOUD <mahmoud.amri@outlook.com>
2016-09-19 23:06:06 +0100

    Added Files

---
+++ Assets/TopDown_AI/Scripts/Singleton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+using UnityEngine;
+using System.Collections;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    protected static T instance;
+
+    /**
+      Returns the instance of this singleton.
+   */
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = (T)FindObjectOfType(typeof(T));
+            }
+
+            return instance;
+        }
+    }
+}
---
+++ Assets/TopDown_AI/Scripts/SoundManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent(typeof(AudioSource))]
+
+public class SoundManager : Singleton<SoundManager>
+{
+
+    [HideInInspector]
+    public float _destinationVolume = 1.0f;
+
+    public AudioClip _cartouch;
+    public AudioClip _explosion;
+
+    private AudioSource _audioSource;
+
+    void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        _audioSource.volume = 1.0f;
+    }
+
+    void Update()
+    {
+
+       // _audioSource.volume = Mathf.Lerp(_audioSource.volume, _destinationVolume, Time.deltaTime);
+    }
+
+    public void PlayCartouchSound()
+    {
+        _audioSource.PlayOneShot(_cartouch);
+    }
+
+    public void PlayExplosionSound()
+    {
+        _audioSource.PlayOneShot(_explosion);
+    }
+
+}
